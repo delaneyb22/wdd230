b4d7dbf4ab3ffa18787ce9fe1191e5a59c32a046
delaneyb22 <delaneybaker8@gmail.com>
2023-02-01 14:42:27 -0600

    menu

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -3,8 +3,11 @@ const date = new Date(document.lastModified);
     document.getElementById('last-modified').innerHTML ="Last Modified:"+date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
 
 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
 function toggleMenu(){
-    document.getElementById("navigation").classList.toggle("open");
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
-const x= document.getElementById("hamburger-button");
-x.onclick = toggleMenu;
