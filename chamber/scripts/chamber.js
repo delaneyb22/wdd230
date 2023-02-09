45e374ac35a957afa1511913ef324a657dc6a5f3
delaneyb22 <114117065+delaneyb22@users.noreply.github.com>
2023-02-08 18:53:27 -0600

    Update chamber.js

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,6 +1,6 @@
 document.querySelector("#last-modified").textContent = `Last Modification: ${document.lastModified}`;
-const date = new Date(document.lastModified);
-    document.getElementById('last-modified').innerHTML ="Last Modified:"+date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+const lastModifiedDate = new Date(document.lastModified);
+    document.getElementById('last-modified').innerHTML ="Last Modified:"+lastModifiedDate.getMonth()+"-"+lastModifiedDate.getDate()+"-"+lastModifiedDate.getFullYear()+" "+lastModifiedDate.getHours()+":"+lastModifiedDate.getMinutes()+":"+lastModifiedDate.getSeconds();
 
 
 const mainnav = document.querySelector('.navigation')
@@ -11,6 +11,38 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 function toggleMenu(){
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
-//make the date like Feb 13 2023
-const topDate=new Date();
-document.querySelector("#date").textContent=topDate;
+
+//header date
+const currentDate = new Date();
+ const month = currentDate.toLocaleString('default', { month: 'long' });
+    document.getElementById('currentdate').innerHTML =month+" "+currentDate.getDate()+", "+currentDate.getFullYear();
+
+//display banner mon tues
+
+let bannerMessage; 
+const bannerDay  = new Date().getDay();        // current date/time
+            
+switch(bannerDay) {
+    case 0://sunday
+        bannerMessage = '';
+        break;
+    case 1:
+        bannerMessage = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m';
+        break;
+    case 2://tuesday
+        bannerMessage = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m';
+        break;
+    case 3://wed
+        bannerMessage = '🤝🏼 Come join us for the chamber';
+        break;
+    case 4:
+        bannerMessage = '';
+        break;
+    case 5:
+        bannerMessage = '';
+        break;
+    case 6:
+        bannerMessage = '';
+        break;
+        }
+document.getElementById("banner").innerHTML = bannerMessage;
